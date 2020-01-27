eb1fdb62340fc174ad0360c1294887b5d38a2e4c
Shivaranjini <m.shivaranjini@gmail.com>
2020-01-27 12:37:53 +0530

    Update haralik.py

--- modules/descriptors/haralik.py
+++ modules/descriptors/haralik.py
@@ -5,11 +5,12 @@ import numpy as np
 
 class Haralik:
 
-			
+	def __init__(self, ignore_zeros = True):
+		self.ignore_zeros = ignore_zeros	
 
 	def describe(self, image):		
-		haralik  = mahotas.features.haralick(image).mean(axis=0)		
+		haralik  = mahotas.features.haralick(image, self.ignore_zeros).mean(axis=0)		
 		return haralik
 
 	def getFeatureVectorSize(self):
-		return 0 
+		return 13 
